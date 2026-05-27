4216f1029a9a0942065bcbefa633be94f09b5720
rjrpv <richardson.rj@gmail.com>
2026-05-26 23:57:48 -0700

    Fix compatibility issues

--- custom_components/camilladsp/config_flow.py
+++ custom_components/camilladsp/config_flow.py
@@ -99,9 +99,6 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        super().__init__(config_entry)
-
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
